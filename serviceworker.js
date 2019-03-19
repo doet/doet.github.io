1cc4f318f3b91f96364dcdf42965e8d0a174f5c4
unknown <dit_doet@yahoo.com>
2019-03-19 14:36:48 +0700

    coba

--- serviceworker.js
+++ serviceworker.js
@@ -51,30 +51,29 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-//
-//
-// self.addEventListener('fetch', function(event) {
-//   var request = event.request
-//   var url = new URL(request.url)
-//   if(url.original === location.origin){
-//     event.respondWith(
-//       caches.match(request).then(function(response){
-//         return response || fetch(request)
-//       })
-//     )
-//   } else {
-//     event.respondWith(
-//       caches.open('products-cache').then(function(cache){
-//         return fetch(request).then(function(liveResponse){
-//           cache.put(request, liveResponse.clone())
-//           return liveResponse
-//         }).catch(function(){
-//           return cache.match(request).then(function(response){
-//             if(response)return response
-//             return caches.match('/fallback.json')
-//           })
-//         })
-//       })
-//     );
-//   }
-// });
+
+self.addEventListener('fetch', function(event) {
+  var request = event.request
+  var url = new URL(request.url)
+  if(url.original === location.origin){
+    event.respondWith(
+      caches.match(request).then(function(response){
+        return response || fetch(request)
+      })
+    )
+  } else {
+    event.respondWith(
+      caches.open('products-cache').then(function(cache){
+        return fetch(request).then(function(liveResponse){
+          cache.put(request, liveResponse.clone())
+          return liveResponse
+        }).catch(function(){
+          return cache.match(request).then(function(response){
+            if(response)return response
+            return caches.match('/fallback.json')
+          })
+        })
+      })
+    );
+  }
+});
