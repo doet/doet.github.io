de26606ef2c784914fbb2d4418dc3074a76c7e8e
unknown <dit_doet@yahoo.com>
2019-03-19 12:29:44 +0700

    upload logo

--- js/main.js
+++ js/main.js
@@ -1,68 +1,68 @@
 $(document).ready(function(){
-  var _url="https://my-json-server.typicode.com/doet/pwaapi/products"
-  var dataResults = ""
-  var catResults = ""
-  var categories = []
-  var items
-
-  function renderPage(data){
-    $.each(data, function(key, items){
-      _cat = items.category
-      dataResults += "<div>"
-                    +"<h3>"+ items.nama +"</h3>"
-                    +"<p>"+ _cat +"</p>"
-                  +"</div>";
-      if($.inArray(_cat,categories)==-1){
-        categories.push(_cat)
-        catResults+="<option value'"+ _cat +"'>"+ _cat+"</options>"
-      }
-    })
-    $('#products').html(dataResults)
-    $('#cat_select').html("<options value='all'>semua</option>"+catResults)
-  }
-
-  var networkDataRecived = false
-
-  var networkUpdate = fetch(_url).then(function(response){
-    return response.json()
-  }).then(function(data){
-    networkDataRecived = true
-    renderPage(data)
-  })
-  caches.match(_url).then(function(response){
-    if(!response) throw Error('no datra no cache')
-    return response.json()
-  }).then(function(data){
-    if(!networkDataRecived){
-      renderPage(data)
-      console.log('data render')
-    }
-  }).catch(function(){
-    return networkUpdate
-  })
-
-  //fungsi filter
-  $('#cat_select').on('change',function(){
-    updateProduct($(this).val())
-  })
-
-  function updateProduct(cat){
-    var dataResults = ""
-    var _newUrl = _url
-    if (cat!='all')
-    _newUrl = _url +"?category="+cat
-
-    $.get(_newUrl, function(data){
-      $.each(data, function(key, items){
-        _cat = items.category
-        dataResults += "<div>"
-                      +"<h3>"+ items.nama +"</h3>"
-                      +"<p>"+ _cat +"</p>"
-                    +"</div>";
-      })
-      $('#products').html(dataResults)
-    })
-  }
+  // var _url="https://my-json-server.typicode.com/doet/pwaapi/products"
+  // var dataResults = ""
+  // var catResults = ""
+  // var categories = []
+  // var items
+  //
+  // function renderPage(data){
+  //   $.each(data, function(key, items){
+  //     _cat = items.category
+  //     dataResults += "<div>"
+  //                   +"<h3>"+ items.nama +"</h3>"
+  //                   +"<p>"+ _cat +"</p>"
+  //                 +"</div>";
+  //     if($.inArray(_cat,categories)==-1){
+  //       categories.push(_cat)
+  //       catResults+="<option value'"+ _cat +"'>"+ _cat+"</options>"
+  //     }
+  //   })
+  //   $('#products').html(dataResults)
+  //   $('#cat_select').html("<options value='all'>semua</option>"+catResults)
+  // }
+  //
+  // var networkDataRecived = false
+  //
+  // var networkUpdate = fetch(_url).then(function(response){
+  //   return response.json()
+  // }).then(function(data){
+  //   networkDataRecived = true
+  //   renderPage(data)
+  // })
+  // caches.match(_url).then(function(response){
+  //   if(!response) throw Error('no datra no cache')
+  //   return response.json()
+  // }).then(function(data){
+  //   if(!networkDataRecived){
+  //     renderPage(data)
+  //     console.log('data render')
+  //   }
+  // }).catch(function(){
+  //   return networkUpdate
+  // })
+  //
+  // //fungsi filter
+  // $('#cat_select').on('change',function(){
+  //   updateProduct($(this).val())
+  // })
+  //
+  // function updateProduct(cat){
+  //   var dataResults = ""
+  //   var _newUrl = _url
+  //   if (cat!='all')
+  //   _newUrl = _url +"?category="+cat
+  //
+  //   $.get(_newUrl, function(data){
+  //     $.each(data, function(key, items){
+  //       _cat = items.category
+  //       dataResults += "<div>"
+  //                     +"<h3>"+ items.nama +"</h3>"
+  //                     +"<p>"+ _cat +"</p>"
+  //                   +"</div>";
+  //     })
+  //     $('#products').html(dataResults)
+  //   })
+  // }
 })
 
 //pwa
