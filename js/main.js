559415d7292e2c2202b51043762311acf5de4700
unknown <dit_doet@yahoo.com>
2019-03-19 12:32:56 +0700

    upload logo

--- js/main.js
+++ js/main.js
@@ -68,12 +68,12 @@ $(document).ready(function(){
 //pwa
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    // navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+    navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
       // Registration was successful
       // console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    // }).catch(function(err) {
+    }).catch(function(err) {
       // registration failed :(
       // console.log('ServiceWorker registration failed: ', err);
-    // });
+    });
   });
 }
